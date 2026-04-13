4ab52b124c254aecc65b3f370af4165f94d9e125
ChiruCadiz <cadiz.ch@gmail.com>
2026-04-12 20:23:30 -0400

    Incorporación Sprint 1
    Se incorporan todo lo realizado para el primer incremento

--- app/main.py
+++ app/main.py
@@ -27,7 +27,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://frontend-production-98d34.up.railway.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
